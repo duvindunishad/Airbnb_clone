8aa7391e6d5f928db2d3ddd60eb7310ff92d9967
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-09-25 11:23:52 +0530

    create the user menu and search nevbar

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 
 const Navbar = () => {
@@ -9,9 +10,10 @@ const Navbar = () => {
             <div className="py-4 border-b-[1px]">
                 <Container>
                     <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-                    </div>
                     <Logo/>
                     <Search/>
+                    <UserMenu/>
+                    </div>
                 </Container>
             </div>
         </div>
